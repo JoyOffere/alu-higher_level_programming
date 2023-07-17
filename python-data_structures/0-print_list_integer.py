25edeb43ed3ff37eef14ca1e26b786e9247dabef
Joy Offere <j.offere@alustudent.com>
2023-07-17 05:10:47 -0700

    0. Print a list of integers

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-def print_list_integer(my_list=[]):
-    for i in my_list:
-        print("{0:d}".format(i))
-        print("{:d}".format(i))
-    return 0
+print_list_integer = __import__('0-print_list_integer').print_list_integer
+
+my_list = [1, 2, 3, 4, 5]
+print_list_integer(my_list)
