d726588e6bbebacd3c00c43c9186622856d33f46
Joy Offere <j.offere@alustudent.com>
2023-07-17 13:29:25 -0700

    Safe integer print with error message

--- python-exceptions/100-safe_print_integer_err.py
+++ python-exceptions/100-safe_print_integer_err.py
@@ -1,17 +1,11 @@
 #!/usr/bin/python3
-safe_print_integer = __import__('1-safe_print_integer').safe_print_integer
+import sys
 
-value = 89
-has_been_print = safe_print_integer(value)
-if not has_been_print:
-    print("{} is not an integer".format(value))
 
-value = -89
-has_been_print = safe_print_integer(value)
-if not has_been_print:
-    print("{} is not an integer".format(value))
-
-value = "School"
-has_been_print = safe_print_integer(value)
-if not has_been_print:
-    print("{} is not an integer".format(value))
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+        return True
+    except Exception as e:
+        print("Exception: {}".format(e), file=sys.stderr)
+        return False
