a673226ab2a38efeabe5e5fca51e8ea44964e7cf
Joy Offere <j.offere@alustudent.com>
2023-07-25 09:14:15 -0700

    0. My first square

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 # 0-square.py
 """
-This is a module or python file that has a class called Square and this class Square performs an action.
+This is a module or python file that has a class
+called Square and this class Square performs an action.
 """
 
 
 class Square():
     """
- This is an empty class called Square that defines square without doing nothing.
+    This is an empty class called Square
+    that defines square without doing nothing.
     """
     pass
