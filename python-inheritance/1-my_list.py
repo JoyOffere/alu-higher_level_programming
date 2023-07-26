d08015dbf336f3b055f3561514204257260331c5
Joy Offere <j.offere@alustudent.com>
2023-07-26 11:56:17 -0700

    Correction of "1. My list"

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,13 +1,29 @@
 #!/usr/bin/python3
-"""Creates a Mylist class that inherits from list."""
+# 1-my_list.py
+"""This is a python module
+that inherits from th built-in
+class List and creates a sorted
+function to sort the list not
+in-place
+"""
 
 
 class MyList(list):
-    """Mylist class => A sub class of list"""
+    """This is my class
+    MyList that inherits from
+    the built in class list
+    an creates a sorted function
+    """
+    # An initialization method of an empty list is done by the parent class
     def __init__(self):
-        """initializes the object"""
         super().__init__()
 
     def print_sorted(self):
-        """prints the sorted list"""
+        """This is the sorted function
+        that sorts the the the instance
+        of the list mylist which inherits
+        the abilities of the class list
+        and the method .sort() and sorts
+        the instance not in place
+        """
         print(sorted(self))
