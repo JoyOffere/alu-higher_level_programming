7fe68ebac551cabdd225363c859a1cb243cca88a
Joy Offere <j.offere@alustudent.com>
2023-07-27 07:25:12 -0700

    4. Only sub class of

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-inherits_from = __import__('4-inherits_from').inherits_from
+'''Defines inherits_from
+'''
 
-a = True
-if inherits_from(a, int):
-    print("{} inherited from class {}".format(a, int.__name__))
-if inherits_from(a, bool):
-    print("{} inherited from class {}".format(a, bool.__name__))
-if inherits_from(a, object):
-    print("{} inherited from class {}".format(a, object.__name__))
+
+def inherits_from(obj, a_class):
+    '''Checks if obj is an instance of a class that
+    inherits directly/indirectly from a_class
+    '''
+    return issubclass(type(obj), a_class) and type(obj) != a_class
