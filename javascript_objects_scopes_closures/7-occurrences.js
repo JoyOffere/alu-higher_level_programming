a7b03518f06c3271381a6226e483a4db30e1a70d
Joy Offere <j.offere@alustudent.com>
2023-09-30 15:19:14 -0700

    7. Occurrences

--- javascript_objects_scopes_closures/7-occurrences.js
+++ javascript_objects_scopes_closures/7-occurrences.js
@@ -1,18 +1,10 @@
 #!/usr/bin/node
-const square = require('./5-square.js');
-const Square = class extends square {
-  charPrint (c) {
-    if (c === undefined) {
-      this.print();
-    } else {
-      for (let h = 0; h < this.height; h++) {
-        let shape = '';
-        for (let w = 0; w < this.width; w++) {
-          shape = shape + c;
-        }
-        console.log(shape);
-      }
+exports.nbOccurences = function (list, searchElement) {
+  let occurenceCount = 0;
+  for (let i = 0; i < list.length; i++) {
+    if (list[i] === searchElement) {
+      occurenceCount++;
     }
   }
+  return occurenceCount;
 };
-module.exports = Square;
